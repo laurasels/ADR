5d906fbe24edc3249f225c6b5c10209cec3f83e7
laurasels <47247722+laurasels@users.noreply.github.com>
2019-12-16 10:37:20 +0100

    nummering afbeeldingen

--- detect_video3.py
+++ detect_video3.py
@@ -22,7 +22,9 @@ flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when sav
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
 
 
+
 def main(_argv):
+    i=1
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -66,6 +68,7 @@ def main(_argv):
         img_in = tf.expand_dims(img, 0)
         img_in = transform_images(img_in, FLAGS.size)
 
+        #calculate time it takes to predict an object
         t1 = time.time()
         boxes, scores, classes, nums = yolo.predict(img_in)
         t2 = time.time()
@@ -75,15 +78,15 @@ def main(_argv):
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         img = cv2.putText(img, "Time: {:.2f}ms".format(sum(times)/len(times)*1000), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+
         if FLAGS.output:
             out.write(img)
+
             #car = 2, truck = 7
-            if 2 in classes:
-                print('car present at', "Time: {:.2f}ms".format(sum(times)/len(times)*1000))
-                cv2.imwrite('./detected_images/detected_cars/detectedcar_' + str("Time: {:.2f}ms".format(sum(times)/len(times)*1000)) +'.png', img)
             if 7 in classes:
-                print('truck present at', "Time: {:.2f}ms".format(sum(times)/len(times)*1000))
-                cv2.imwrite('/.detected_images/detected_trucks/detectedtruck_' + str("Time: {:.2f}ms".format(sum(times)/len(times)*1000)) + '.png', img)
+                print('truck detected')
+                cv2.imwrite('./detected_images/detected_trucks/detectedtruck_' + str(i) +'.png', img)
+                i+=1
 
         cv2.imshow('output', img)
         if cv2.waitKey(1) == ord('q'):
